fix: fix gradient update

main.py
@@ -5,7 +5,7 @@ import argparse
 import torch
 
 from metafscil.models import get_model
-from metafscil.dataset import get_pretrain_dataloader, SequentialTaskSampler
+from metafscil.dataset import get_pretrain_dataloader, SequentialTaskSampler, EpisodeSampler
 from metafscil.trainer import Pretrain, MetaFSCIL
 
 
@@ -61,7 +61,15 @@ def metatrain(args):
 
 
 def evaluate(args):
-    pass
+    print('Evaluate...')
+
+    sampler = EpisodeSampler()
+
+    model = get_model(args.model, 60).to(args.device)
+    model.load_state_dict(torch.load(f'models/{args.model}/metatrain.pth'))
+
+    trainer = MetaFSCIL(model, sampler, args)
+    trainer.meta_test()
 
 
 if __name__ == '__main__':
@@ -74,4 +82,4 @@ if __name__ == '__main__':
         if args.pretrain:
             pretrain(args)
         metatrain(args)
-    # evaluate(args)
+    evaluate(args)

metafscil/dataset.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.transforms.functional import resize
 from torchvision.io import read_image, ImageReadMode
+from sklearn.utils import shuffle
 
 
 def get_metadata(path: str):
@@ -68,7 +69,7 @@ def get_pretrain_dataloader(
             imgs.append(img)
             labels.append(i)
     dataset = MiniImagenetDataset(imgs, labels, transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=6, drop_last=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=12, pin_memory=True)
     return dataloader
 
 
@@ -137,14 +138,19 @@ class SequentialTaskSampler:
             self.query_labels += [self.shuffle_classes.index(cls)] * len(self.query_pools[cls])
 
         self.support_loader = self._get_loader(self.support_imgs, self.support_labels, min(len(self.support_imgs), 128))
-        self.query_loader = self._get_loader(self.query_imgs, self.query_labels, self.n_sample_query)
+        self.query_loader = self._get_loader(self.query_imgs, self.query_labels,
+                                             self.n_sample_query, max_samples=self.n_query)
 
         self.support_iter = iter(self.support_loader)
         self.query_iter = iter(self.query_loader)
 
-    def _get_loader(self, imgs, labels, batch_size):
+    def _get_loader(self, imgs, labels, batch_size, max_samples=None):
+        imgs, labels = shuffle(imgs, labels)
+        if max_samples is not None:
+            imgs = imgs[:max_samples]
+            labels = labels[:max_samples]
         dataset = MiniImagenetDataset(imgs, labels)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=2)
         return dataloader
 
     def sample_query(self):
@@ -186,15 +192,15 @@ class EpisodeSampler:
         # training set
         self.support_imgs = []
         self.support_labels = []
-        imgs, cls = get_metadata(f"metadata/miniimagenet/session_{self.session + 1}.txt")
-        self.classes += cls
-        for cls in self.classes:
+        imgs, clss = get_metadata(f"metadata/mini_imagenet/session_{self.session + 1}.txt")
+        self.classes += clss
+        for cls in clss:
             self.support_imgs += imgs[cls][:]
             self.support_labels += [self.classes.index(cls)] * len(imgs[cls])
 
         # testing set
-        imgs, _ = get_metadata(f"metadata/miniimagenet/test_{self.session + 1}.txt")
-        for cls in self.classes:
+        imgs, _ = get_metadata(f"metadata/mini_imagenet/test_{self.session + 1}.txt")
+        for cls in clss:
             self.query_imgs += imgs[cls][:]
             self.query_labels += [self.classes.index(cls)] * len(imgs[cls])
 
@@ -205,7 +211,7 @@ class EpisodeSampler:
 
     def _get_loader(self, imgs, labels, batch_size):
         dataset = MiniImagenetDataset(imgs, labels)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=2)
         return dataloader
 
     def sample_support(self):

metafscil/trainer.py
@@ -5,7 +5,7 @@ from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from .utils import disable_grad, compute_accuracy
+from .utils import enable_grad, disable_grad, compute_accuracy
 
 
 class Pretrain:
@@ -78,7 +78,6 @@ class MetaFSCIL:
         self.device = args.device
 
         self.loss = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
 
         self.writer = SummaryWriter(f"{args.log_dir}/{args.model}")
 
@@ -146,7 +145,9 @@ class MetaFSCIL:
 
                 self.fast_adapt()
                 self.model.classifier = copy.deepcopy(self.fast_model.classifier)
-                loss, acc = self.meta_update()
+                fast_params = list(self.fast_model.feature_extractor.parameters())
+                del self.fast_model
+                loss, acc = self.meta_update(fast_params)
                 total_loss += loss
                 total_acc += acc
 
@@ -155,20 +156,16 @@ class MetaFSCIL:
         self.writer.add_scalar('Meta/train_loss', total_loss / self.n_sessions, epoch)
         self.writer.add_scalar('Meta/train_accuracy', total_acc / self.n_sessions, epoch)
 
-    def meta_update(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        optimizer.load_state_dict(self.optimizer.state_dict())
+    def meta_update(self, params):
+        disable_grad(self.model, ['classifier'])
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr)
         data, target = self.task_sampler.sample_query()
         data, target = data.to(self.device), target.to(self.device)
-        output = self.fast_model(data)
+        output = self.model(data, params)
         loss = self.loss(output, target)
         optimizer.zero_grad()
-        grads = torch.autograd.grad(loss, self.model.parameters(), allow_unused=True)
-        for p, g in zip(self.model.named_parameters(), grads):
-            if 'classifier' not in p[0]:
-                p[1].grad = g
+        loss.backward()
         optimizer.step()
-        self.optimizer.load_state_dict(optimizer.state_dict())
 
         return loss.item(), compute_accuracy(output, target)
 